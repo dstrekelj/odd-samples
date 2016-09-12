0590e251a70ec6f65f91017af5edbbf6fa607233
Domagoj Štrekelj <domagojstrekelj@gmail.com>
2016-09-12 07:32:25 +0200

    Minor update to all samples

--- Sample04/src/odd/samples/sample04/Main.hx
+++ Sample04/src/odd/samples/sample04/Main.hx
@@ -4,8 +4,13 @@ import odd.Context;
 
 class Main
 {
+    #if odd_target_android
+    static inline var WIDTH : Int = 160;
+    static inline var HEIGHT : Int = 120;
+    #else
     static inline var WIDTH : Int = 640;
     static inline var HEIGHT : Int = 480;
+    #end
 
     static function main() : Void
     {
